23e33339711c3d492296c70156653ace8c18b9ef
Brent Stees <brent.stees@sungard.com>
2012-09-21 11:28:35 -0400

    Ruby Installation
    All scss files are /app/assets/stylesheets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SgUi::Application.config.secret_token = 'ac9b9dc4ad13748d229fafb86a790a1d10398c948a264256e3d87be2fe11d10a929083ac35ff8256fb28e7f21c3dc3086fece0e48a7ab4a8a1be7b5e9fca179e'
